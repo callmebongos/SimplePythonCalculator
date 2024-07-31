8630a1b198180367c79df58f2d10d3696858cc1c
MB2k05 <mbakerr2005@gmail.com>
2024-07-31 14:29:37 +0300

    Initial Commit. Version 0.0.2

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,7 +1,3 @@
-# Very basic Python calculator
-# Zero external libraries
-# Version 0.0.01
-
 def add(x, y):
     return x + y
 
@@ -18,18 +14,18 @@ def divide(x, y):
         return x / y
 
 def calculator():
-    print("Select Operation:")
+    print("Select operation:")
     print("1. Add")
     print("2. Subtract")
     print("3. Multiply")
     print("4. Divide")
-
+    
     choice = input("Enter choice(1/2/3/4): ")
-
+    
     if choice in ['1', '2', '3', '4']:
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
-
+        
         if choice == '1':
             print(f"The result is: {add(num1, num2)}")
         elif choice == '2':
@@ -38,8 +34,8 @@ def calculator():
             print(f"The result is: {multiply(num1, num2)}")
         elif choice == '4':
             print(f"The result is: {divide(num1, num2)}")
-        else:
-            print("Invalid Input.")
+    else:
+        print("Invalid input")
 
-if  __name__ == "__main__":
+if __name__ == "__main__":
     calculator()
